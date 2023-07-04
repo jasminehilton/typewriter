7a9a5c561eef9485d2affa11954013909c8a86bf
jasmine hilton <jasminehilton1@live.com>
2023-07-04 20:59:31 +0000

    added \n

--- typewriter.js
+++ typewriter.js
@@ -2,7 +2,7 @@ const sentence = "hello there from lighthouse labs";
 
 for (let i = 0; i < sentence.length; i++) {
   setTimeout(() => {
-    process.stdout.write(sentence[i]);
+    process.stdout.write(sentence[i]) + "\n";
     
   }, i * 50);
 }
